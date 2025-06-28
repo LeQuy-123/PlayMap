23ab2c0f08e7cf071aba302f1b4e1a01c0dbb20a
quy.le <ledaosiquy@gmail.com>
2025-06-28 19:07:20 +0700

    add map box

--- src/features/map/MapScreen.tsx
+++ src/features/map/MapScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, View  } from 'react-native';
+import { SafeAreaView, StyleSheet  } from 'react-native';
 import Mapbox from '@rnmapbox/maps';
 
 Mapbox.setAccessToken('pk.eyJ1IjoibGVxdXkiLCJhIjoiY2tzc2hpaDJlMGZ3ajMwcGg0eTZlYTVudCJ9.2DvsLCt5Z4o3ySQHThnq3w');
@@ -8,26 +8,22 @@ const MapScreen = () => {
     
     return (
         <SafeAreaView style={[ styles.container]}>
-            <View style={styles.container}>
-                <Mapbox.MapView style={styles.map} />
-            </View>
+            <Mapbox.MapView style={styles.map} />
         </SafeAreaView>
     );
 };
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-       
     },
     page: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
-   
     map: {
         flex: 1
-      }
+    }
 });
 
 export default MapScreen;
