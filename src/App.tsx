d9f4c347a27a01aeddae747ca9bd9eb640078cdd
quy.le <ledaosiquy@gmail.com>
2025-07-07 19:06:13 +0700

    init load metadata

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text } from 'react-native';
 import '~localization';
-import { Provider } from 'react-redux';
+import { Provider, useDispatch } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from '~store/store';
+import { AppDispatch, persistor, store } from '~store/store';
 import { MainNavigator } from './navigation/MainNavigator';
 import { ToastProvider } from '~utils/ToastProvider';
 import { useConnectionListener } from '~utils/ConnectionListener';
 import { BottomNotificationProvider } from '~utils/BottomNotificationProvider';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { fetchSports } from '~store/slices/sportSlice';
 // const { SplashScreen } = NativeModules;
 
+
+const AppInit = ({ children }: { children: React.ReactNode }) => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        await dispatch(fetchSports()).unwrap();
+      } catch (err) {
+        console.error('Failed to load metadata:', err);
+      } finally {
+        // SplashScreen.hide(); // only hide when ready
+      }
+    };
+    init();
+  }, []);
+
+  return <>{children}</>;
+};
 function App(): React.JSX.Element {
   useConnectionListener();
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     if (Platform.OS === 'android') { // handle android splash screen native
-  //       SplashScreen.hide();
-  //     }
-  //   }, 500);
-  // }, []);
   return (
     <Provider store={store}>
       <SafeAreaProvider>
@@ -28,7 +41,9 @@ function App(): React.JSX.Element {
           <BottomNotificationProvider>
             <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
               <GestureHandlerRootView>
-                <MainNavigator />
+                <AppInit>
+                  <MainNavigator />
+                </AppInit>
               </GestureHandlerRootView>
             </PersistGate>
           </BottomNotificationProvider>
--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,8 +1,4 @@
 import axios from 'axios';
-import NetInfo from '@react-native-community/netinfo';
-import { store } from '~store/store';
-import { toast } from '~utils/ToastProvider';
-import { logout, setAuth } from '~store/slices/authSlice';
 import Config from 'react-native-config';
 
 
@@ -14,21 +10,21 @@ const apiClient = axios.create({
 // Attach access token to requests
 apiClient.interceptors.request.use(
   async config => {
-    const state = store.getState();
-    const token = state.auth.token;
+    // const state = store.getState();
+    // const token = state.auth.token;
 
-    const netInfo = await NetInfo.fetch();
-    if (!netInfo.isConnected) {
-      toast({
-        msg: 'No internet connection',
-        type: 'error',
-      });
-      return Promise.reject({__handled: true});
-    }
+    // const netInfo = await NetInfo.fetch();
+    // if (!netInfo.isConnected) {
+    //   toast({
+    //     msg: 'No internet connection',
+    //     type: 'error',
+    //   });
+    //   return Promise.reject({__handled: true});
+    // }
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+    // if (token) {
+    //   config.headers.Authorization = `Bearer ${token}`;
+    // }
 
     return config;
   },
@@ -36,86 +32,71 @@ apiClient.interceptors.request.use(
 );
 
 // Refresh token handling
-let isRefreshing = false;
-let failedQueue: any[] = [];
+// let isRefreshing = false;
+// let failedQueue: any[] = [];
 
-const processQueue = (error: any, token: string | null = null) => {
-  failedQueue.forEach(prom => {
-    if (error) {prom.reject(error);}
-    else {prom.resolve(token);}
-  });
-  failedQueue = [];
-};
+// const processQueue = (error: any, token: string | null = null) => {
+//   failedQueue.forEach(prom => {
+//     if (error) {prom.reject(error);}
+//     else {prom.resolve(token);}
+//   });
+//   failedQueue = [];
+// };
 
 // Response Interceptor
 apiClient.interceptors.response.use(
   response => response,
   async error => {
 
-    const originalRequest = error.config;
-    const status = error.response?.status;
-    const state = store.getState();
+    // const originalRequest = error.config;
+    // const status = error.response?.status;
+    // const state = store.getState();
 
-    const shouldTryRefresh =
-      status === 401 && !originalRequest._retry && state.auth.refreshToken;
+    // const shouldTryRefresh =
+    //   status === 401 && !originalRequest._retry && state.auth.refreshToken;
 
-    if (shouldTryRefresh) {
-      originalRequest._retry = true;
+    // if (shouldTryRefresh) {
+    //   originalRequest._retry = true;
 
-      if (isRefreshing) {
-        return new Promise((resolve, reject) => {
-          failedQueue.push({
-            resolve: (token: string) => {
-              originalRequest.headers.Authorization = 'Bearer ' + token;
-              resolve(apiClient(originalRequest));
-            },
-            reject: (err: any) => reject(err),
-          });
-        });
-      }
+    //   if (isRefreshing) {
+    //     return new Promise((resolve, reject) => {
+    //       failedQueue.push({
+    //         resolve: (token: string) => {
+    //           originalRequest.headers.Authorization = 'Bearer ' + token;
+    //           resolve(apiClient(originalRequest));
+    //         },
+    //         reject: (err: any) => reject(err),
+    //       });
+    //     });
+    //   }
 
-      isRefreshing = true;
+    //   isRefreshing = true;
 
-      try {
-        // const res = await  AuthService.refreshTokenApi(state.auth.refreshToken!);
-        const { data } = await apiClient.post('/auth/refresh', {
-          refreshToken: state.auth.refreshToken,
-        });
-        const {accessToken, refreshToken, user} = data;
+    //   try {
+    //     // const res = await  AuthService.refreshTokenApi(state.auth.refreshToken!);
+    //     const { data } = await apiClient.post('/auth/refresh', {
+    //       refreshToken: state.auth.refreshToken,
+    //     });
+    //     const {accessToken, refreshToken, user} = data;
 
-        store.dispatch(setAuth({user, token: accessToken, refreshToken}));
-        processQueue(null, accessToken);
+    //     store.dispatch(setAuth({user, token: accessToken, refreshToken}));
+    //     processQueue(null, accessToken);
 
-        originalRequest.headers.Authorization = `Bearer ${accessToken}`;
-        return apiClient(originalRequest);
-      } catch (refreshErr: any) {
-        processQueue(refreshErr, null);
-        store.dispatch(logout());
-        toast({
-          msg: 'Session expired. Please log in again.',
-          type: 'error',
-        });
-        return Promise.reject({...refreshErr, __handled: true});
-      } finally {
-        isRefreshing = false;
-      }
-    }
-
-    // Your existing error handling
-    // if (status === 401 ) {
-    //   store.dispatch(logout());
-    //   notify('Session expired. Please log in again.', 'error');
-    //   return Promise.reject({...error, __handled: true});
-    // } else if (status === 500) {
-    //   notify('Server error. Please try again later.', 'error');
-    //   return Promise.reject({...error, __handled: true});
-    // } else if (status === 403) {
-    //   notify('You do not have permission to access this resource.', 'warning');
-    //   return Promise.reject({...error, __handled: true});
-    // } else if (status >= 400 && status < 500) {
-    //   notify(error.response?.data?.message || 'Request failed', 'error');
-    //   return Promise.reject({...error, __handled: true});
+    //     originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+    //     return apiClient(originalRequest);
+    //   } catch (refreshErr: any) {
+    //     processQueue(refreshErr, null);
+    //     store.dispatch(logout());
+    //     toast({
+    //       msg: 'Session expired. Please log in again.',
+    //       type: 'error',
+    //     });
+    //     return Promise.reject({...refreshErr, __handled: true});
+    //   } finally {
+    //     isRefreshing = false;
+    //   }
     // }
+ 
 
     return Promise.reject(error);
   },
